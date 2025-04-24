302eb7bd10ce5cae12c7cdd4ccdeb2b052b81fed
Pau Vilardell <pau.vilardell@uvic.cat>
2025-04-24 14:17:00 +0000

    good changes

--- module/src/main/java/cat/uvic/teknos/dam/aureus/CoinCollection.java
+++ module/src/main/java/cat/uvic/teknos/dam/aureus/CoinCollection.java
@@ -1,22 +1,4 @@
 package cat.uvic.teknos.dam.aureus;
 
-public class CoinCollection {
-    private int coinId;
-    private int collectionId;
-
-    public int getCoinId() {
-        return coinId;
-    }
-
-    public void setCoinId(int coinId) {
-        this.coinId = coinId;
-    }
-
-    public int getCollectionId() {
-        return collectionId;
-    }
-
-    public void setCollectionId(int collectionId) {
-        this.collectionId = collectionId;
-    }
+public interface CoinCollection {
 }
